80c1052e7460a5c00a69c4f276ebaf921779fac0
linjx-ustc1106 <47167445+linjx-ustc1106@users.noreply.github.com>
2019-05-30 19:56:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     
     # Training configuration.
     parser.add_argument('--batch_size', type=int, default=8, help='mini-batch size')
-    parser.add_argument('--num_iters', type=int, default=300000, help='number of total iterations for training D')
+    parser.add_argument('--num_iters', type=int, default=200000, help='number of total iterations for training D')
     parser.add_argument('--num_iters_decay', type=int, default=100000, help='number of iterations for decaying lr')
     parser.add_argument('--g_lr', type=float, default=0.0001, help='learning rate for G')
     parser.add_argument('--d_lr', type=float, default=0.0001, help='learning rate for D')
